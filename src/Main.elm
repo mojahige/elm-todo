213de84b617754558be9652f7d27e12f1ea1fcf2
mojahige <mojahige@gmail.com>
2020-09-12 13:24:16 +0900

    Update TODO!
    Applied tailwindcss. and updated HTML markup

--- src/Main.elm
+++ src/Main.elm
@@ -2,8 +2,8 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
+import Html.Attributes exposing (checked, class, disabled, placeholder, title, type_, value)
+import Html.Events exposing (onClick, onInput, onSubmit)
 
 
 main : Program () Model Msg
@@ -26,10 +26,14 @@ type alias Todo =
     }
 
 
+type alias Memos =
+    List Todo
+
+
 type alias Model =
     { id : Int
     , input : String
-    , memos : List Todo
+    , memos : Memos
     }
 
 
@@ -102,50 +106,90 @@ view model =
             , onSubmit Submit
             ]
             [ input
-                [ class "bg-white focus:outline-none focus:shadow-outline border-4 border-blue-800 rounded-r-none rounded-l-lg py-2 px-4 block w-full appearance-none leading-normal"
+                [ class "text-2xl font-bold bg-white focus:outline-none focus:shadow-outline border-4 border-blue-800 rounded-r-none rounded-l-lg py-2 px-4 block w-full appearance-none leading-normal"
                 , value model.input
+                , placeholder "Input TODO ⌨️"
                 , onInput Input
                 ]
                 []
             , button
-                [ class "bg-blue-900 text-white font-bold py-2 px-4 rounded-r-lg rounded-l-none"
+                [ class "flex-shrink-0 bg-blue-900 text-white font-bold py-2 px-4 rounded-r-lg rounded-l-none"
                 , class
-                    (if String.length model.input > 0 then
+                    (if hasInput model.input then
                         "hover:bg-blue-800"
 
                      else
                         "opacity-75 cursor-not-allowed"
                     )
-                , disabled (String.length model.input == 0)
+                , disabled (not (hasInput model.input))
                 ]
-                [ text "Submit" ]
+                [ text "Add 😎" ]
             ]
-        , ul [] (List.map todoListComponent model.memos)
+        , if hasTodos model.memos then
+            div [ class "flex justify-center mt-6" ]
+                [ ul
+                    [ class "divide-y divide-gray-400" ]
+                    (List.indexedMap todoListComponent model.memos)
+                ]
+
+          else
+            p [ class "mt-6 text-4xl font-bold text-gray-600 text-center" ] [ text "Empty todo 👀" ]
         ]
 
 
-todoListComponent : Todo -> Html Msg
-todoListComponent todo =
-    li [] [ todoComponent todo ]
+hasInput : String -> Bool
+hasInput input =
+    String.length input > 0
+
+
+hasTodos : Memos -> Bool
+hasTodos memos =
+    List.length memos > 0
+
+
+todoListComponent : Int -> Todo -> Html Msg
+todoListComponent index todo =
+    li
+        [ class "p-2"
+        , class
+            (if index > 0 then
+                "mt-2"
+
+             else
+                ""
+            )
+        ]
+        [ todoComponent todo ]
 
 
 todoComponent : Todo -> Html Msg
 todoComponent todo =
-    div []
+    div [ class "flex items-center" ]
         [ label
-            [ if todo.done then
-                class "hoge"
-
-              else
-                class ""
-            ]
+            [ class "flex items-center mr-auto text-2xl font-bold" ]
             [ input
                 [ type_ "checkbox"
                 , checked todo.done
                 , onClick (Check todo.id (not todo.done))
                 ]
                 []
-            , text todo.text
+            , span
+                [ class "ml-2"
+                , class
+                    (if todo.done then
+                        "line-through"
+
+                     else
+                        ""
+                    )
+                ]
+                [ text todo.text ]
             ]
-        , button [ type_ "button", onClick (Remove todo.id) ] [ text "❎" ]
+        , button
+            [ type_ "button"
+            , class "ml-8 text-2xl"
+            , title "Click to remove"
+            , onClick (Remove todo.id)
+            ]
+            [ text "❎" ]
         ]
